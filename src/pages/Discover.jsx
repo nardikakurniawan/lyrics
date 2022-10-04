7656f8cd5caa5da09ea5351eb7893aa74f8b6192
Nardika Kurniawan <nardikakurniawan20@gmail.com>
2022-10-04 14:29:46 +0700

    Song Card Component | Play/Pause Functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "POP";
 
@@ -32,7 +37,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song.key} seng={song} i={i}></SongCard>
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
         ))}
       </div>
     </div>
